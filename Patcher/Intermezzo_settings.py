f52a4c685d02a42be5c4e8cc603f22585554595b
Krummers <krummers10@gmail.com>
2023-08-26 21:50:01 +0200

    Add clear screen to settings menu

--- Patcher/Intermezzo_settings.py
+++ Patcher/Intermezzo_settings.py
@@ -1,5 +1,11 @@
 import Intermezzo_functions as im
 import os
+import platform as pf
+
+if pf.uname()[0] == "Windows":
+    command = "cls"
+else:
+    command = "clear"
 
 while True:
     # Print all settings
@@ -18,6 +24,7 @@ while True:
         case "A":
             print("The current value is:", im.Setting("directory").get_value())
             print("Enter the directory where the files should be moved to after patching, separated by a comma.")
+            print("Do not include the drive name. Start from the first folder of your location.")
             folders = str(input("Directory: ")).split(",")
             
             drive = os.path.splitdrive(os.getcwd())[0]
@@ -27,21 +34,25 @@ while True:
                 directory = os.path.join(directory, folder)
             
             im.Setting("directory").set_value(directory)
+            os.system(command)
         case "B":
             print("The current value is:", im.Setting("iso-rename").get_value())
             iso_rename = im.question("Rename an ISO after patching?")
             
             im.Setting("iso-rename").set_value(iso_rename)
+            os.system(command)
         case "C":
             print("The current value is:", im.Setting("pycache").get_value())
             delete_pycache = im.question("Delete the \"__pycache__\" folder after patching?")
 
             im.Setting("pycache").set_value(delete_pycache)
+            os.system(command)
         case "D":
             print("The current value is:", im.Setting("riivo-suffix").get_value())
             riivo_suffix = im.question("Add a suffix to Riivolution builds after patching?")
             
             im.Setting("riivo-suffix").set_value(riivo_suffix)
+            os.system(command)
         case "X":
             break
         case other:
