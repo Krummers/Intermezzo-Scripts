3f0c05c81017e867b2368395c50eee7dca382ad9
swampatha <kingfunkykong@gmail.com>
2024-02-06 15:29:28 -0500

    Updated LE-CODE binaries.

--- Patcher/Intermezzo.py
+++ Patcher/Intermezzo.py
@@ -60,11 +60,11 @@ while True:
     
     if choice in cs.types[:2]:
         prefix = "mkw-intermezzo"
-        patch2_download = "https://cdn.discordapp.com/attachments/870580346033430549/1203114020106805300/patch2.tar"
+        patch2_download = "https://cdn.discordapp.com/attachments/870580346033430549/1204523462081650738/patch2.tar"
         break
     elif choice in cs.types[2:]:
         prefix = ""
-        patch2_download = "https://cdn.discordapp.com/attachments/870580346033430549/1203114063949987950/patch2.tar"
+        patch2_download = "https://cdn.discordapp.com/attachments/870580346033430549/1204523527924093039/patch2.tar"
         break
     else:
         print("This is not an option. Please try again.")
