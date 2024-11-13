96e81d45c9202cc8e8720ba3b3071db90b8a8cbb
Krummers <101184909+Krummers@users.noreply.github.com>
2024-11-13 16:17:35 +0100

    Rewrite part of create

--- Krummers-Generator/Modules/enumerables.py
+++ Krummers-Generator/Modules/enumerables.py
@@ -6,3 +6,18 @@ class Action(en.Enum):
     Delete = en.auto()
     Exit = en.auto()
 
+class CreateAction(en.Enum):
+    Create = en.auto()
+    Edit = en.auto()
+    Exit = en.auto()
+
+class CreateEditAction(en.Enum):
+    Add = en.auto()
+    Remove = en.auto()
+    Redownload = en.auto()
+    Exit = en.auto()
+
+class TrackType(en.Enum):
+    Normal = en.auto()
+    Wish = en.auto()
+    Nintendo = en.auto()
--- Krummers-Generator/create.py
+++ Krummers-Generator/create.py
@@ -1,36 +1,22 @@
 import os
 import script_utilities.file as fl
+import script_utilities.functions as ft
 
 import common as cm
 import folders as fd
 
+import Modules.enumerables as eb
+
 selections = fd.get_folder("Selections")
 
-def select_option(folders: list[str]) -> str:
-    """Select what to do for a track selection."""
-    
-    while True:
-        option = input("Create a new track selection, edit an existing one or exit? (N, E or X): ").lower()
-        
-        if option == "e" and not folders:
-            print("There do not exist any selections.")
-        elif option == "n":
-            return "new"
-        elif option == "e":
-            return "edit"
-        elif option == "x":
-            return "exit"
-        else:
-            print("This is not an option. Please try again.")
-
-def new_selection(folders: list[str]) -> str:
-    """Select name for new selection."""
+def new_selection() -> str:
+    """Lets the user enter a name for a new selection."""
     
     while True:
         print("Only alpha-numeric characters are allowed, as well as dashes.")
         selection = input("What to name this track selection? (Enter the name): ")
         
-        if not selection.replace("-", "").replace(" ", "").isalnum():
+        if not selection.replace("-", "").isalnum():
             print("This name is not alpha-numeric. Please try again.")
             continue
         
@@ -43,41 +29,11 @@ def new_selection(folders: list[str]) -> str:
         return selection
 
 def save_tracks(selection: str, trackids: list[cm.TrackID]) -> None:
-    """Save tracks of a given selection."""
+    """Saves a list of tracks to a given selection."""
     
     tracks = fl.PKL(os.path.join(selections.path, selection, "tracks.pkl"))
     tracks.set_value(trackids)
     return
-
-def select_action() -> str:
-    """Select action to perform on a selection."""
-    
-    while True:
-        option = input("Add tracks, remove tracks, redownload information or exit? (A, R, D or X): ").lower()
-        
-        if option == "a":
-            return "add"
-        elif option == "r":
-            return "remove"
-        elif option == "d":
-            return "download"
-        elif option == "x":
-            return "exit"
-        else:
-            print("This is not an option. Please try again.")
-
-def select_track_type() -> str:
-    """Select type to tie to the track."""
-    
-    while True:
-        option = input("Add tracks normally or as a wish? (N or W): ").lower()
-        
-        if option == "n":
-            return "normal"
-        elif option == "w":
-            return "wish"
-        else:
-            print("This is not an option. Please try again.")
 
 def print_trackids(trackids: list[cm.TrackID]) -> None:
     """Print track IDs in a readable way."""
@@ -88,21 +44,24 @@ def print_trackids(trackids: list[cm.TrackID]) -> None:
     if not trackids:
         print("\t* (none)")
 
-def trackid_selector(action: str) -> tuple[int]:
-    """Select tracks that need to be added/removed."""
+def trackid_selector(action: eb.CreateEditAction) -> tuple[int]:
+    """Lets the user select a range of tracks."""
     
-    if action == "add":
-        infinitive = "add"
-        present_participle = "adding"
-        past_participle = "added"
-    elif action == "remove":
-        infinitive = "remove"
-        present_participle = "removing"
-        past_participle = "removed"
+    match action.name:
+        case "Add":
+            infinitive = "add"
+            present_participle = "adding"
+            past_participle = "added"
+        case "Remove":
+            infinitive = "remove"
+            present_participle = "removing"
+            past_participle = "removed"
         
     print(f"What tracks need to be {past_participle}?")
-    print(f"For example, the prompt \"5-9\" will {infinitive} tracks with ID 5, 6, 7, 8 and 9.")
-    print(f"{present_participle.capitalize()} a single track is done by entering \"34\".")
+    print(f"For example, the prompt \"5-9\" will {infinitive} tracks "
+          "with ID 5, 6, 7, 8 and 9.")
+    print(f"{present_participle.capitalize()} a single track is done "
+          "by entering \"34\".")
     
     while True:
         prompt = input("Enter the first and last track ID separated by a dash or a single ID on its own: ")
@@ -186,36 +145,52 @@ def redownload_information(trackids: list[cm.TrackID], selection: str) -> None:
 def main() -> None:
     while True:
         folders = fd.get_selections()
-        option = select_option(folders)
+        actions = list(eb.CreateAction)
+        display = [action.name for action in actions]
+        action = ft.options_question(actions,
+                                     "What action needs to be performed?",
+                                     display)
         
-        if option == "exit":
-            return
-        
-        cm.print_from_list(folders)
-        if option == "new":
-            selection = new_selection(folders)
-        elif option == "edit":
-            selection = cm.select_from_list(folders, "Which selection needs to be edited?", print_menu = False)
+        match action.name:
+            case "Create":
+                cm.print_from_list(folders)
+                selection = new_selection()
+            case "Edit":
+                cm.print_from_list(folders)
+                selection = cm.select_from_list(folders,
+                                                "Which selection needs "
+                                                "to be edited?",
+                                                print_menu = False)
+            case "Exit":
+                return
         
         trackids = cm.load_tracks(selection)
         while True:
-            action = select_action()
+            edit_actions = list(eb.CreateEditAction)
+            display = [edit.name for edit in edit_actions]
+            edit_action = ft.options_question(edit_actions,
+                                              "What action needs to be performed?",
+                                              display)
             
-            if action == "exit":
-                break
+            match edit_action.name:
+                case "Add":
+                    track_types = list(eb.TrackType)[:-1]
+                    display = [ttype.name for ttype in track_types]
+                    track_type = ft.options_question(track_types,
+                                                     "What type should the track get?",
+                                                     display)
+                    print_trackids(trackids)
+                    first, last = trackid_selector(edit_action)
+                    trackids = add_trackids(trackids, selection, track_type, first, last)
+                case "Remove":
+                    print_trackids(trackids)
+                    first, last = trackid_selector(edit_action)
+                    trackids = remove_trackids(trackids, selection, first, last)
+                case "Redownload":
+                    redownload_information(trackids, selection)
+                case "Exit":
+                    break
             
-            if action == "add":
-                track_type = select_track_type()
-                print_trackids(trackids)
-                first, last = trackid_selector(action)
-                trackids = add_trackids(trackids, selection, track_type, first, last)
-            elif action == "remove":
-                print_trackids(trackids)
-                first, last = trackid_selector(action)
-                trackids = remove_trackids(trackids, selection, first, last)
-            elif action == "download":
-                redownload_information(trackids, selection)
-        
             save_tracks(selection, trackids)
 
 if __name__ == "__main__":
